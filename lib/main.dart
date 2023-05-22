f0ac65a710ef5240d9a45f21bbc4af85c80f013f
kjhoon1410 <kjhoon1410@naver.com>
2023-05-22 19:58:19 +0900

    Initial commit // 스플래쉬화면 디자인 절반밖에 못함

---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StartScreen(),
+    );
+  }
+}
+
+class StartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 120,
+              height: 121,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(26),
+                image: DecorationImage(
+                  image: AssetImage('assets/apple.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 200),  // This is to adjust the space between the image and the text
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SplashScreen()),
+                );
+              },
+              child: SizedBox(
+                width: 200,
+                height: 83,
+                child: Text(
+                  "START!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontFamily: "Lexend Peta",
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToSecondScreen();
+  }
+
+  navigateToSecondScreen() async {
+    await Future.delayed(Duration(seconds: 2), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => SecondScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/splash.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 332,
+                height: 64,
+                child: Text(
+                  "식사메뉴를 선정해줄게!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontFamily: "Lexend Peta",
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ),
+              CircularProgressIndicator(),
+              SizedBox(height: 20),
+              Text(
+                "Loading...",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: Text("This is the second screen"),
+      ),
+    );
+  }
+}
+
